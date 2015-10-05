3045bd290dc1e86ad493ea4f714963a48aa9cb08
Svilen Stoicheff <svilen.stoicheff@orbitz.com>
2015-10-05 17:44:12 -0500

    adding sorting capability

--- controllers/flights.js
+++ controllers/flights.js
@@ -21,8 +21,28 @@ request(flightApiOptions, function(err, response, body){
         console.log(err);
     } else if (response.statusCode === 200) {
     		var responseBody = JSON.parse(body);
+            var flightOffers = [], 
+                flightOffersSort = function(){
+                    responseBody.offers.forEach(function(item, index, array){
+                        var newItem = parseInt(item.baseFare);
+                        item.baseFareNum = newItem;
+                        console.log(newItem);
+                        
+                            flightOffers.push(item);
+                     
+                    });
+                };
+                flightOffersSort();
+
+                var flightOffersSorted = flightOffers.sort(function(a,b){
+                    return a.baseFareNum - b.baseFareNum;
+                });
+
+                //console.log(flightOffersSorted);
+
+                //console.log(flightOffers);
     		
-    		res.render('flights', { title: 'Flight Results', flightlegs: responseBody.legs, flightoffers: responseBody.offers, 
+    		res.render('flights', { title: 'Flight Results', flightlegs: responseBody.legs, flightoffers: flightOffersSorted, 
                 departureDate: departureDate, departureAirport: departureAirport, arrivalAirport: arrivalAirport });
     		
     } else {
