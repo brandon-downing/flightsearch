b727983beaed7b1a235b997c34da920cab76d839
Svilen Stoicheff <svilen.stoicheff@orbitz.com>
2015-10-09 09:19:08 -0500

    basic details

--- controllers/flights.js
+++ controllers/flights.js
@@ -46,5 +46,30 @@ request(flightApiOptions, function(err, response, body){
 
 
 module.exports.flightDetail = function(req, res){
-    res.render('flightDetail', {title: 'Flight Detail'});
+    var departureDate = req.query.departureDate, 
+		departureAirport = req.query.departureAirport, 
+		arrivalAirport = req.query.arrivalAirport, 
+        productKey = req.query.productKey,
+        flightDetailApiOptions = {
+    		url: 'https://www.expedia.com:443/api/flight/details?departureDate='+ departureDate +'&departureAirport='+ departureAirport +'&arrivalAirport='+ arrivalAirport +'&productKey='+ productKey,  
+    		method: "GET"
+		};
+        
+    request(flightDetailApiOptions, function(err, response, body){
+        console.log(flightDetailApiOptions.url);
+        if(err){
+            console.log(err);
+        } else if (response.statusCode === 200) {
+                var responseBody = JSON.parse(body);
+                   console.log(responseBody);
+                   
+                    
+                res.render('flightDetail', {title: 'Flight Detail', flightDetail: responseBody});
+                
+        } else {
+            console.log(response.statusCode);
+        }
+    });
+    
+    
 }
