291a527f142652261fd2fdad3f1b5c5767a111a3
Svilen Stoicheff <svilen.stoicheff@orbitz.com>
2015-10-16 14:47:54 -0500

    Populate dropdowns dynamically

--- public/javascripts/fs-lib.js
+++ public/javascripts/fs-lib.js
@@ -1,10 +1,24 @@
 var FS = {
 	populateOrigDest: function(){
-		$('#origin-destination').html(airportsCodesNames[departureAirport] +' - '+ airportsCodesNames[arrivalAirport]);
+		if (typeof departureAirport !== 'undefined' && typeof arrivalAirport !== 'undefined'){
+			$('#origin-destination').html(airportsCodesNames[departureAirport] +' - '+ airportsCodesNames[arrivalAirport]);
+		}
+	}, 
+	
+	populateAirportDropdowns: function(){
+		if(typeof airportsCodesNames !== 'undefined' && $('#departureAirport') && $('#arrivalAirport'))
+		$.each(airportsCodesNames, function(key, value){
+			var option = '<option value="'+key+'">'+value+'</option';
+			$('#departureAirport').append(option);
+			$('#arrivalAirport').append(option);
+
+		});
 	}
 };
 
 
 $(function(){
+	//functions to run on DOM ready
 	FS.populateOrigDest();
+	FS.populateAirportDropdowns();
 });
